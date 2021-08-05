54b0901146276c8b230adbf0117256b58d3ebe3a
elthorne <46898565+elthorne@users.noreply.github.com>
2021-08-05 15:45:14 +0100

    Added reference to Blaise REACT components and created landing and upload page. Added instrument name form field and csv upload button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ReactElement} from "react";
+import {Switch, Route, Link, useLocation} from "react-router-dom";
+import UploadPage from "./Components/GenerataUACs/GenerateUACs";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+    Footer,
+    Header,
+    BetaBanner,
+    ONSPanel,
+    NotProductionWarning
+} from "blaise-design-system-react-components";
+
+const divStyle = {
+    minHeight: "calc(67vh)"
+};
+
+interface Location {
+    state: any
+}
+
+function App(): ReactElement {
+    const location = useLocation();
+
+    return (
+        <>
+            {
+                (window.location.hostname.includes("dev")) && <NotProductionWarning/>
+            }
+            <BetaBanner/>
+            <Header title={"Create UACs for case samples"}/>
+            <div style={divStyle} className="page__container container">
+                <main id="main-content" className="page__main">
+                    <Switch>
+                        <Route path="/upload">
+                            <UploadPage/>
+                        </Route>
+                        <Route path="/">
+
+                            <ul className="list list--bare list--inline u-mt-m">
+                                <li className="list__item">
+                                    <Link to="/upload" id="deploy-questionnaire-link">
+                                        Upload sample
+                                    </Link>
+                                </li>
+                            </ul>
+
+                            <ONSPanel>
+                                <p>
+                                    This is a landing page. Construction is on hold until we've finished the <Link
+                                    to="/upload" id="deploy-questionnaire-link">upload sample</Link> page. Leave us
+                                    alone until then!
+                                </p>
+                            </ONSPanel>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
