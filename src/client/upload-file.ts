1c3435d86e9d4f95166fdc2ed9d1a6527a3f3887
jamie kerr <jamiestephenkerr@gmail.com>
2021-08-08 17:58:06 +0100

    Ugh

---
+++ src/client/upload-file.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export async function uploadFile(instrumentName: string | undefined, file: File | undefined) {
+    if (instrumentName === undefined || instrumentName === null) {
+        console.error("Instrument name not supplied")
+        return null;
+    }
+
+    if (file === undefined || file === null) {
+        console.error("File not supplied")
+        return null;
+    }
+
+    const data = new FormData();
+    data.append('instrumentName', instrumentName);
+    data.append('file', file);
+
+    const config = {headers: {'Content-Type': 'multipart/form-data'}}
+
+    await axios.post('/api/v1/upload', data, config)
+}
